843d6c97a4b054b0e5da38cb69072244468204d0
ArtRosh <artem.rosh17@gmail.com>
2025-01-11 16:47:39 -0800

    looping-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Экспорт функции, если нужно
+  module.exports = { writeCards, countDown };
+
+
+  function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  module.exports = { writeCards };  // Экспорт функции
