f3b5c28e5c796ef165607845b058f286a0fdb00c
ArtRosh <artem.rosh17@gmail.com>
2025-02-19 20:39:50 -0800

    for

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // Code your solutions in this file
 
-function countDown(number) {
-    while (number >= 0) {
-      console.log(number);
-      number--;
-    }
+function countDown(number){
+  for (let i = number; i >= 0; i--){
+    console.log(i);
   }
+}
   
-  // Экспорт функции, если нужно
-  module.exports = { writeCards, countDown };
+ 
+  
 
 
   function writeCards(names, gift) {
     return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
   }
   
-  module.exports = { writeCards };  // Экспорт функции
+  module.exports = { writeCards, countDown };
